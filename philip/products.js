4a058fe39bd74063cfd5dbd10fa1a09c136fb758
Oore <goodnessoluseyi@gmail.com>
2026-05-28 20:37:22 +0100

    add more api

--- philip/products.js
+++ philip/products.js
@@ -3,3 +3,9 @@ fetch('https://fakesstoreapi.com/products')
     .then(data => {
         console.log(data);
     })
+    
+fetch('https://fakesstoreapi.com/sticky')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
