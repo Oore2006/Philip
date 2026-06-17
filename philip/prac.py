a7365b3a9cb25b1523d52265ee8a3addc259c12a
Oore <goodnessoluseyi@gmail.com>
2026-06-17 16:49:22 +0100

    new function

--- philip/prac.py
+++ philip/prac.py
@@ -3,3 +3,11 @@ def age:
         print("You are a minor."),
     else  age >= 18 and age < 65:
         print(" You are can franchise.")
+
+def grade:
+    if grade < 70:
+        print("You can do better next time."),
+    else grade >= 70 and grade < 90:
+        print("You did well."),
+    else grade >= 90:
+        print("You did excellent.")
